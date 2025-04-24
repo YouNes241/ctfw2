c22957502cebe18bbb6e46a8a498bbdf213a0343
Ouaammou <younes.ouaammou@etu.univ-orleans.fr>
2025-04-24 14:52:06 +0200

    Question 2

--- README.md
+++ README.md
@@ -16,3 +16,34 @@ Les propriétés pour représenter les informations d'observations :
 * Longitude => `Float`
 * Animal Observé => `Animal (ManyToOne)`
 * Description => `Text`
+
+# Question 2
+
+```bash
+symfony composer install
+symfony console doctrine:database:create
+symfony console make:entity Animal
+symfony console make:entity Observation
+symfony console make:migration
+symfony console doctrine:migrations:migrate
+symfony server:start --no-tls --listen-ip=0.0.0.0 --d
+```
+
+Points d'entrée pour Animal : 
+
+```
+GET /api/animals
+POST /api/animals
+GET /api/animals/{id}
+DELETE /api/animals/{id}
+PATCH /api/animals/{id}
+```
+
+Points d'entrée pour Observation : 
+```
+GET /api/observations
+POST /api/observations
+GET /api/observations/{id}
+DELETE /api/observations/{id}
+PATCH /api/observations/{id}
+```
---
+++ backend/src/Entity/Observation.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ObservationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ObservationRepository::class)]
+#[ApiResource]
+class Observation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $time = null;
+
+    #[ORM\Column]
+    private ?float $latitude = null;
+
+    #[ORM\Column]
+    private ?float $longitude = null;
+
+    #[ORM\ManyToOne(inversedBy: 'observations')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Animal $animal = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getTime(): ?\DateTimeInterface
+    {
+        return $this->time;
+    }
+
+    public function setTime(\DateTimeInterface $time): static
+    {
+        $this->time = $time;
+
+        return $this;
+    }
+
+    public function getLatitude(): ?float
+    {
+        return $this->latitude;
+    }
+
+    public function setLatitude(float $latitude): static
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    public function getLongitude(): ?float
+    {
+        return $this->longitude;
+    }
+
+    public function setLongitude(float $longitude): static
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    public function getAnimal(): ?Animal
+    {
+        return $this->animal;
+    }
+
+    public function setAnimal(?Animal $animal): static
+    {
+        $this->animal = $animal;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
